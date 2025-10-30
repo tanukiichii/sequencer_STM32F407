8f3dc4956d331da6d16d6182926fa291946efd3b
Bubik <101990903+tanukiichii@users.noreply.github.com>
2025-10-30 16:21:37 +0300

    Debugged modes switch

---
+++ Core/Src/buttons.h
@@ -0,0 +1,27 @@
+#ifndef __BUTTONS_H__
+#define __BUTTONS_H__
+
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+#define MODE_GPIO GPIOB
+#define MODE_Pin GPIO_PIN_11
+
+#define BTN_GPIO GPIOB
+#define BTN_Pin GPIO_PIN_10
+
+//Info functions
+uint8_t is_mode_button_pressed(void);
+uint8_t is_mode_button_held(void);
+
+//Handle buttons functions
+void handle_mode_button(void);
+void handle_tm1638_buttons(void);
+void handle_clear_button (void);
+
+//combo info
+uint8_t is_combo_press_detected(void);
+uint8_t is_mode_button_held(void);
+uint8_t get_combo_sample_id(void);
+
+#endif //BUTTONS_H
--- Core/Src/pattern.h
+++ Core/Src/pattern.h
@@ -1,25 +1,28 @@
 #ifndef PATTERN_H
 #define PATTERN_H
 
-#include "main.h"
 #include "audio.h"
-//#include "samples.h"
+#include "main.h"
+#include "modes.h"
 #include "stm32f4xx_hal.h"
-//#include "timers.h"
 #include <string.h>
 
 #define NUM_SLOTS 16
 #define NUM_SAMPLES 16
+#define MAX_SAMPLES_PER_SLOT 4  // ???????? ??????? ? ????? ?????
 
 typedef struct 
 {
-    uint8_t activated_samples[NUM_SLOTS][NUM_SAMPLES]; 
+    uint8_t sample_count[NUM_SLOTS];  // ?????????? ??????? ? ?????? ?????
+    uint8_t samples[NUM_SLOTS][MAX_SAMPLES_PER_SLOT];  // ?????? ??? ??????? ?????
 } Pattern;
 
 extern Pattern current_pattern;
 extern uint8_t current_slot;
 extern uint8_t is_recording_pattern;
 extern uint8_t is_playing_pattern;
+static uint8_t editing_sample_id = 0;
+static uint8_t is_editing_mode = 0;
 
 //Initialisation func
 void init_pattern(void);
@@ -36,7 +39,20 @@ void stop_pattern_playback(void);
 void clear_pattern(void);
 void record_sample_to_slot(uint8_t slot, uint8_t sample_id);
 void remove_sample_from_slot(uint8_t slot, uint8_t sample_id);
+void remove_all_samples_from_slot(uint8_t slot);
 void play_samples_for_slot(uint8_t slot);
 uint8_t is_sample_in_slot(uint8_t slot, uint8_t sample_id);
+uint8_t get_sample_count_in_slot(uint8_t slot);
+
+//Pattern editing functions
+void enter_sample_edit_mode(uint8_t sample_id);
+void exit_sample_edit_mode(void);
+uint8_t is_editing_sample(void);
+uint8_t get_editing_sample_id(void);
+
+//Sample blinking
+void stop_sample_blinking(void);
+void start_sample_blinking(void);
+uint8_t is_sample_blinking(void);
 
 #endif // PATTERN_H
