e85d2216bf3ca0127a54674b4c648a5c3f5c2ae5
Bubik <101990903+tanukiichii@users.noreply.github.com>
2025-11-24 16:28:09 +0300

    Audio output via CS43L22

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "adc.h"
 #include "dac.h"
 #include "dma.h"
 #include "i2c.h"
@@ -29,14 +28,16 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "audio.h"
-#include "bpm.h"
+//#include "bpm.h"
 #include "buttons.h"
 #include "effects.h"
 #include "leds.h"
 #include "modes.h"
+#include "MY_CS43L22.h"
 #include "pattern.h"
 #include "tm1638.h"
 #include <math.h>
+/* USER CODE END I
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,10 +47,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define PI 3.14159f
-
-#define F_SAMPLE 48000.0f
-#define F_OUT    1200.0f
+#define F_OUT    200.0f
+#define PI 3.14f
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -60,8 +59,11 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-volatile uint16_t adc_value = 0;
-volatile uint16_t bpm;
+float MySinVal;
+float sample_dt;
+uint16_t sample_N;
+uint16_t i_t;
+uint32_t MyDacVal;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -72,7 +74,9 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+extern uint8_t btn_pushed = 0;
+uint16_t I2S_dummy[4];
+volatile uint16_t bpm = 120;
 /* USER CODE END 0 */
 
 /**
@@ -104,31 +108,37 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_I2S3_Init();
-  MX_I2C1_Init();
-  MX_TIM2_Init();
   MX_DAC_Init();
+  MX_I2S3_Init();
+  MX_TIM2_Init();
   MX_TIM3_Init();
-  MX_ADC1_Init();
-  MX_TIM4_Init();
-  MX_TIM5_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  
+  CS43_Init(hi2c1, MODE_ANALOG);  // ?????: MODE_ANALOG!
+  CS43_SetVolume(80);
+  CS43_Enable_RightLeft(CS43_RIGHT_LEFT);
+  CS43_Start();
+  
   TM1638_Init();
   init_pattern();
   LEDS_Init();
   mode_handler_init();
   Reverb_Init();
   
+  HAL_I2S_Transmit_DMA(&hi2s3, (uint16_t *)I2S_dummy, 4);
+    
   HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
   
-  HAL_TIM_Base_Start_IT(&htim3);
   HAL_TIM_Base_Start_IT(&htim2);
   
+  HAL_TIM_Base_Start_IT(&htim3);  
+  //LEDS_AllOn();
+ 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
   while (1)
   {
     SEQ_MODE current_mode = get_current_mode();
@@ -136,11 +146,9 @@ int main(void)
     handle_tm1638_buttons();
     handle_clear_button();
     handle_fx_button();
-    update_edit_mode_leds();
+    update_edit_mode_leds();    
     
-    bpm = UpdateBPMFromPotentiometer();
-    
-
+    update_global_fx();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -165,13 +173,12 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -195,7 +202,6 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* Prevent unused argument(s) compilation warning */
@@ -208,8 +214,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     {
       if(get_current_mode() != MUTE) 
       {
-            audio_callback();
-        }
+        audio_callback();
+      }
     }
     else if(htim->Instance == TIM3) 
     {    
@@ -236,6 +242,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
         }
     } 
 }
+
+
 /* USER CODE END 4 */
 
 /**
